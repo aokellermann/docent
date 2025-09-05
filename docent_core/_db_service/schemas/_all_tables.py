088af315948b3f2c62a116ca04d8f66ca5c727e0
Kevin Meng <kevin.meng.01@gmail.com>
2025-09-04 17:59:25 -0700

    Remove unused diffing tables, fix chat sessions index names (#349)

--- docent_core/_db_service/schemas/_all_tables.py
+++ docent_core/_db_service/schemas/_all_tables.py
@@ -1,7 +1,8 @@
 import docent_core._db_service.schemas.base as base
 import docent_core.docent.db.schemas.chart as chart
+import docent_core.docent.db.schemas.chat as chat
 import docent_core.docent.db.schemas.refinement as refinement
 import docent_core.docent.db.schemas.rubric as rubric
 import docent_core.docent.db.schemas.tables as tables
 
-__all__ = ["base", "rubric", "refinement", "chart", "tables"]
+__all__ = ["base", "rubric", "refinement", "chart", "tables", "chat"]
