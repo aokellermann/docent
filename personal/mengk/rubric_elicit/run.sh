5ffc76138ea6e96014843f9bd0adf593faee89f0
Kevin Meng <kevin.meng.01@gmail.com>
2026-02-28 14:09:57 -0800

    backup

--- personal/mengk/rubric_elicit/run.sh
+++ personal/mengk/rubric_elicit/run.sh
@@ -77,9 +77,9 @@
 python3 label_elicitation.py \
     117967cf-d3bb-42e9-947d-eee82e97738f \
     433f5140-10cd-4d10-b192-0d4b01f665bb \
-    --label-num-samples 10 \
+    --label-num-samples 50 \
     --top-n 10 \
-#     --user-data-json outputs/user_data_20260225_185652_gitignore.json
+    --user-data-json outputs/user_data_20260228_220614_gitignore.json
 
 # python3 rubric_bon.py \
 #     117967cf-d3bb-42e9-947d-eee82e97738f \
